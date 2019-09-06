52efb595f9fe67002e0d481fdd0cb130a00dbb73
Artan Muzhaqi <artanmuzhaqi@gmail.com>
2019-09-06 13:36:28 -0400

    implement size(), isEmpty(), findPrevious(),findLast()

--- SLL.js
+++ SLL.js
@@ -25,5 +25,43 @@ display = (list) => {
         currentNode = currentNode.next
     }
 }
+size = (list) => {
+    let currentNode = list.head;
+    let x = 0
+    while (currentNode !== null) {
+        x++;
+        currentNode = currentNode.next
+    }
+    return x;
+}
+isEmpty = (list) => {
+    return list.head === null ? true : false;
+}
+findPrevious = (item, list) => {
+    let currNode = list.head;
+    if (!list.head) {
+        return null;
+    }
+    while (currNode.next.value !== item) {
+        if (currNode.next === null) {
+            return null;
+        }
+        else {
+            currNode = currNode.next;
+        }
+    }
+    return currNode.value
+}
+findLast = (list) => {
+    let currNode = list.head;
+    while (currNode.next !== null) {
+        currNode = currNode.next;
+    }
+    return currNode.value
+}
 main();
 display(SLL)
+console.log(size(SLL))
+console.log(isEmpty(SLL))
+console.log(findPrevious('Kat', SLL))
+console.log(findLast(SLL))
